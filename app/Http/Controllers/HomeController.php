97cccdd0a9afce9351718cb044d8e2a41c1264e8
maximus64 <admin@khoahoang.com>
2016-11-27 02:34:10 +0000

    oops

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('jwt.auth');
+        $this->middleware('jwt.auth');
     }
 
     /**
