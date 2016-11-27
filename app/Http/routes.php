ca6333ecc11c4fe5176775a47f74c96aaea10005
jlundstrom <github@jonl.us>
2016-11-26 19:28:54 -0500

    Added /api/home to go to /home

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,7 @@ Route::group(['prefix' => 'api'], function()
 {
     Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
     Route::post('authenticate', 'AuthenticateController@authenticate');
+    Route::get('home', 'HomeController@index');
 
     // Handles registering
     Route::post('register', 'AuthenticateController@register');
