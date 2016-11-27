b3a487baa9a0b2698549cd4c80beb35aa91b1f73
Mark Paxson <mdpaxson1@knights.ucf.edu>
2016-11-27 00:12:05 +0000

    added files

---
+++ public/app/components/search/directives/selectDoc/selectDoc.directive.js
@@ -0,0 +1,33 @@
+(function(){
+
+ var app = angular.module('app.search');
+
+	//controls the groups assigned to a user and how they are displayed
+/*
+    var CurrentGroup = function($scope)
+	{
+
+		//use some data service to do a rest call to a database
+
+		  $scope.users = [
+			{id:1, text:"Harambe"},
+            {id:2, text:"Trump"},
+            {id:3, text:"Pepe"},
+            {id:1, text:"Memzzz"},
+            {id:2, text:"DOGE"},
+            {id:3, text:"DOGEDOGE"}
+        ];
+
+	};
+    CurrentGroup.$inject = ['$scope'];
+*/
+ 	app.directive('searchSelectDoc', function(){
+		return{
+			restrict: 'E',
+			templateUrl: "/app/components/group/directives/currentGroup/currentGroupView.html",
+            controller: "SearchController",
+            controllerAs: 'searchCtrl'
+		};
+	});
+
+})();
---
+++ public/app/components/search/search.controller.js
@@ -0,0 +1,16 @@
+(function(){
+
+    'use strict';
+    var app = angular.module('app.search');
+
+    app.controller('SearchController',SearchController );
+
+    SearchController.$inject = ['$scope', 'HomeData'];
+    function GroupController($scope, HomeData){
+        console.log("Useless right now")   
+    }
+      
+  
+
+
+})();
