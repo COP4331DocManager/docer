7cb01496aaa545bff20a3e0fe3b962b2d902790e
Mark Paxson <mdpaxson1@knights.ucf.edu>
2016-11-29 17:32:57 +0000

    added search page functionality

--- public/app/components/search/search.controller.js
+++ public/app/components/search/search.controller.js
@@ -8,19 +8,12 @@
     SearchController.$inject = ['$scope', '$http'];
 
     function SearchController($scope, $http){
-        $http.get('/api/home')
-                .success(function (data) {
-                    $scope.data = data;
-                    $scope.slides = data['documents'];
-                       $scope.getSlide = function(slideIndex){
-                                console.log(slideIndex);
-                                console.log($scope.slides[$scope.current.slide]);
-                                return $scope.slides[$scope.current.slide];
-                        };
-            }).error(function (data) {
-              $scope.data = "SHEEEET";
-            });   
+        $scope.current = {};
+        $scope.current.slide = 0;
         
+        $scope.getSlide = function(slideIndex){
+            return $scope.searchResults[$scope.current.slide];
+        };
     };
     
 })();
